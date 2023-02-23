841e0685de29bebc0200a1401aa63ac3e39432d1
Anchal Rathore <76997901+anchal0404@users.noreply.github.com>
2023-02-23 14:34:54 +0530

    Add files via upload

---
+++ 02-Welcome-to-Python/objects-strings.py
@@ -0,0 +1,14 @@
+"sushi"
+'sushi' #An immutable sequence of text characters.
+"warriorsfan123!"
+"$500"
+"You're right"
+" "
+""
+'' #empty strings
+"5" #string
+5 #integer
+""" //Multi line String //
+HelLLO I m AAshi
+
+"""
---
+++ 02-Welcome-to-Python/the-print-function-I-single-argument.py
@@ -0,0 +1,5 @@
+"""Funtions are a sequence of the one or more steps, or a procedure a routine ,a series of instructions
+Can be resuse easily"""
+print("hello")
+print("9-5")
+print("3"+"4")
